30608aa3b009c4e7694cbbd85fe829c2731ca53b
João Gabriel Franco <117865521+jgfranco95@users.noreply.github.com>
2023-05-16 18:47:23 +0000

    aula 1605

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+  alunos=[
+    {nome: 'marcio', telefone:"123", ausente: true}, 
+    {nome: 'luis', telefone:"143", ausente: false},
+    {nome: 'luisa', telefone:"153", ausente: false},
+    {nome: 'lucas', telefone:"163", ausente: true}
+  ];
+
+  
+ 
+
+
 }
