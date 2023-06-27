3d8fa5846c04facf9cc08f7c1e355855a156ae37
João Gabriel Franco <117865521+jgfranco95@users.noreply.github.com>
2023-06-27 22:42:16 +0000

    minha calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports:[CommonModule, IonicModule],
+
 })
 export class HomePage {
-  constructor() {}
+  
+  valor = '';
+
+  // ele acumula o valor do botao para realizar o calculo
+  acumularValor(valor_botao : string){
+    this.valor += valor_botao;
+  }
+
+  // ele limpa os numeros na tela
+  limparValor(){
+    this.valor = '';
+  }
+
+  // realiza os calculos basicos, aonde o eval foi usado para  retornar um resultado string
+  calcularValor(){
+    const resultado = eval(this.valor)
+    this.valor = resultado.toString()
+  }
+// o simbolo +/- ele inverte as operaçoes
+  inverterValor(){
+    const valorInvertido = String(eval(this.valor) * (-1))
+    this.valor = valorInvertido
+  }
+
+  // realiza o calculo de porcentagem
+  calcularPorcentagem(){
+    const valorPorcentagem = eval(this.valor) / 100
+    this.valor = valorPorcentagem.toString()
+    this.valor += '*'
+  }
+
 }
